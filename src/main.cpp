3516d8dc6928a9d4590a5b71d5f7169de0d88666
Christian Hanner <christian.hanner@gmail.com>
2026-02-03 19:49:47 +0100

    Add other cat

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 int main() {
     std::cout << "Welcome to the farm" << '\n';
     const Cat myCat("Misse");
-    std::cout << "Here lives " << myCat.getName() << '\n';
+    const Cat myOtherCat("Majsan");
+    std::cout << "Here lives " << myCat.getName() << " and "
+              << myOtherCat.getName() << '\n';
     return 0;
 }
