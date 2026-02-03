f37c83ff6f27a48b09cf7253e874f7c5d3dc5eb8
Christian Hanner <christian.hanner@gmail.com>
2026-02-03 22:06:48 +0100

    Update clang-format

--- src/cat/cat.cpp
+++ src/cat/cat.cpp
@@ -1,10 +1,17 @@
 #include "cat.h"
 
 Cat::Cat(std::string name, int numLegs)
-    : m_name(std::move(name)), m_numLegs(numLegs) {}
+    : m_name(std::move(name)), m_numLegs(numLegs) {
+}
 
-void Cat::setNumLegs(const int numLegs) { m_numLegs = numLegs; }
+void Cat::setNumLegs(const int numLegs) {
+    m_numLegs = numLegs;
+}
 
-int Cat::getNumLegs() const { return m_numLegs; }
+int Cat::getNumLegs() const {
+    return m_numLegs;
+}
 
-std::string Cat::getName() const { return m_name; }
+std::string Cat::getName() const {
+    return m_name;
+}
